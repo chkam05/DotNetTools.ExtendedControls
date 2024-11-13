4ac5ff3107cc020f9b67c429a0f28a784abed835
Kamil Karpiński <kamil.k3@outlook.com>
2024-11-13 22:41:38 +0100

    Adding code for a new version that I've started working on but still requires starting over.

---
+++ MedVersion/NetFramework/Demo/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using chkam.WPF.ControlsEx.NetFramework.Data;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace chkam.WPF.ControlsExDemo.NetFramework
+{
+    public partial class App : Application
+    {
+
+        private void OnAppStartup(object sender, StartupEventArgs e)
+        {
+            //ThemeManager.Instance.ThemeDataContext.ApplyAccentColor(Colors.Green);
+        }
+
+    }
+}
