b36eb10ee06652614c146449a4ce97d405aef757
Kamil Karpiński <kamil.k3@outlook.com>
2024-11-17 22:44:37 +0100

    Started working on ScrollBarEx

---
+++ NetFramework/chkam05.Tools.ControlsEx/ScrollBarEx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace chkam05.Tools.ControlsEx
+{
+    public class ScrollBarEx : ScrollBar
+    {
+
+        //  METHODS
+
+        #region CONSTRUCTORS
+
+        //  --------------------------------------------------------------------------------
+        /// <summary> Static ScrollBarEx class constructor. </summary>
+        static ScrollBarEx()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ScrollBarEx),
+                new FrameworkPropertyMetadata(typeof(ScrollBarEx)));
+        }
+
+        #endregion CONSTRUCTORS
+
+    }
+}
