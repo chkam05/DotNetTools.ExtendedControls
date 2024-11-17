fb3105275349fb3e42dde4c3699f4d0497e80b36
Kamil Karpiński <kamil.k3@outlook.com>
2024-11-17 01:36:41 +0100

    Working on CheckBoxEx

---
+++ NetFramework/chkam05.Tools.ControlsEx/CheckBoxEx.cs
@@ -0,0 +1,183 @@
+﻿using chkam05.Tools.ControlsEx.Data;
+using chkam05.Tools.ControlsEx.Resources;
+using chkam05.Tools.ControlsEx.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace chkam05.Tools.ControlsEx
+{
+    public class CheckBoxEx : CheckBox
+    {
+
+        //  DEPENDENCY PROPERTIES
+
+        public static readonly DependencyProperty BackgroundInactiveProperty = DependencyProperty.Register(
+            nameof(BackgroundInactive),
+            typeof(Brush),
+            typeof(CheckBoxEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.DarkInactive)));
+
+        public static readonly DependencyProperty BackgroundMouseOverProperty = DependencyProperty.Register(
+            nameof(BackgroundMouseOver),
+            typeof(Brush),
+            typeof(CheckBoxEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.DefaultAccentColorMouseOver)));
+
+        public static readonly DependencyProperty BackgroundPressedProperty = DependencyProperty.Register(
+            nameof(BackgroundPressed),
+            typeof(Brush),
+            typeof(CheckBoxEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.DefaultAccentColorPressed)));
+
+        public static readonly DependencyProperty BorderBrushMouseOverProperty = DependencyProperty.Register(
+            nameof(BorderBrushMouseOver),
+            typeof(Brush),
+            typeof(CheckBoxEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.DefaultAccentColorMouseOver)));
+
+        public static readonly DependencyProperty CheckMarkHeightProperty = DependencyProperty.Register(
+            nameof(CheckMarkHeight),
+            typeof(double),
+            typeof(CheckBoxEx),
+            new PropertyMetadata(16d));
+
+        public static readonly DependencyProperty CheckMarkTypeIconProperty = DependencyProperty.Register(
+            nameof(CheckMarkTypeIcon),
+            typeof(CheckMarkTypeIcon),
+            typeof(CheckBoxEx),
+            new PropertyMetadata(CheckMarkTypeIcon.Default));
+
+        public static readonly DependencyProperty CheckMarkWidthProperty = DependencyProperty.Register(
+            nameof(CheckMarkWidth),
+            typeof(double),
+            typeof(CheckBoxEx),
+            new PropertyMetadata(16d));
+
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register(
+            nameof(CornerRadius),
+            typeof(CornerRadius),
+            typeof(CheckBoxEx),
+            new PropertyMetadata(new CornerRadius(4)));
+
+        public static readonly DependencyProperty ForegroundInactiveProperty = DependencyProperty.Register(
+            nameof(ForegroundInactive),
+            typeof(Brush),
+            typeof(CheckBoxEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.DarkInactive)));
+
+        public static readonly DependencyProperty ForegroundMouseOverProperty = DependencyProperty.Register(
+            nameof(ForegroundMouseOver),
+            typeof(Brush),
+            typeof(CheckBoxEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.DefaultAccentColorForeground)));
+
+        public static readonly DependencyProperty ForegroundPressedProperty = DependencyProperty.Register(
+            nameof(ForegroundPressed),
+            typeof(Brush),
+            typeof(CheckBoxEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.DefaultAccentColorForeground)));
+
+        public static readonly DependencyProperty OpacityInactiveProperty = DependencyProperty.Register(
+            nameof(OpacityInactive),
+            typeof(double),
+            typeof(CheckBoxEx),
+            new PropertyMetadata(0.56d));
+
+
+        //  GETTERS & SETTERS
+
+        public Brush BackgroundInactive
+        {
+            get => (Brush)GetValue(BackgroundInactiveProperty);
+            set => SetValue(BackgroundInactiveProperty, value);
+        }
+
+        public Brush BackgroundMouseOver
+        {
+            get => (Brush)GetValue(BackgroundMouseOverProperty);
+            set => SetValue(BackgroundMouseOverProperty, value);
+        }
+
+        public Brush BackgroundPressed
+        {
+            get => (Brush)GetValue(BackgroundPressedProperty);
+            set => SetValue(BackgroundPressedProperty, value);
+        }
+
+        public Brush BorderBrushMouseOver
+        {
+            get => (Brush)GetValue(BorderBrushMouseOverProperty);
+            set => SetValue(BorderBrushMouseOverProperty, value);
+        }
+
+        public double CheckMarkHeight
+        {
+            get => (double)GetValue(CheckMarkHeightProperty);
+            set => SetValue(CheckMarkHeightProperty, value);
+        }
+
+        public CheckMarkTypeIcon CheckMarkTypeIcon
+        {
+            get => (CheckMarkTypeIcon)GetValue(CheckMarkTypeIconProperty);
+            set => SetValue(CheckMarkTypeIconProperty, value);
+        }
+
+        public double CheckMarkWidth
+        {
+            get => (double) GetValue(CheckMarkWidthProperty);
+            set => SetValue(CheckMarkWidthProperty, value);
+        }
+
+        public CornerRadius CornerRadius
+        {
+            get => (CornerRadius)GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+
+        public Brush ForegroundInactive
+        {
+            get => (Brush)GetValue(ForegroundInactiveProperty);
+            set => SetValue(ForegroundInactiveProperty, value);
+        }
+
+        public Brush ForegroundMouseOver
+        {
+            get => (Brush)GetValue(ForegroundMouseOverProperty);
+            set => SetValue(ForegroundMouseOverProperty, value);
+        }
+
+        public Brush ForegroundPressed
+        {
+            get => (Brush)GetValue(ForegroundPressedProperty);
+            set => SetValue(ForegroundPressedProperty, value);
+        }
+
+        public double OpacityInactive
+        {
+            get => (double)GetValue(OpacityInactiveProperty);
+            set => SetValue(OpacityInactiveProperty, MathUtilities.Clamp(value, 0d, 1d));
+        }
+
+
+        //  METHODS
+
+        #region CONSTRUCTORS
+
+        //  --------------------------------------------------------------------------------
+        /// <summary> CheckBoxEx class constructor. </summary>
+        static CheckBoxEx()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CheckBoxEx),
+                new FrameworkPropertyMetadata(typeof(CheckBoxEx)));
+        }
+
+        #endregion CONSTRUCTORS
+
+    }
+}
---
+++ NetFramework/chkam05.Tools.ControlsEx/Data/CheckMarkTypeIcon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chkam05.Tools.ControlsEx.Data
+{
+    public enum CheckMarkTypeIcon
+    {
+        Default = 0,
+        Filled = 1,
+        FilledCircle = 2,
+        Inline = 3,
+        InlineCircle = 4,
+        Outline = 5,
+        OutlineCircle = 6,
+        MultipleDefault = 7,
+        MultipleFilled = 8,
+        MultipleFilledCircle = 9,
+        MultipleInline = 10,
+        MultipleOutline = 11,
+        MutlipleOutlineCircle = 12
+    }
+}
