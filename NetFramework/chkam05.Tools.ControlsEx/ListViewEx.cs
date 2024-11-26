93556c6d668b06df21ad3f2931450eb352f14934
Kamil Karpiński <kamil.k3@outlook.com>
2024-11-26 22:56:48 +0100

    Finished first part of ListViewEx

--- NetFramework/chkam05.Tools.ControlsEx/ListViewEx.cs
+++ NetFramework/chkam05.Tools.ControlsEx/ListViewEx.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using chkam05.Tools.ControlsEx.Resources;
+using chkam05.Tools.ControlsEx.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace chkam05.Tools.ControlsEx
 {
@@ -13,12 +16,80 @@ namespace chkam05.Tools.ControlsEx
 
         //  DEPENDENCY PROPERTIES
 
+        public static readonly DependencyProperty BackgroundInactiveProperty = DependencyProperty.Register(
+            nameof(BackgroundInactive),
+            typeof(Brush),
+            typeof(ListViewEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.LighInactive)));
 
+        public static readonly DependencyProperty BorderBrushInactiveProperty = DependencyProperty.Register(
+            nameof(BorderBrushInactive),
+            typeof(Brush),
+            typeof(ListViewEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.LighInactive)));
+
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register(
+            nameof(CornerRadius),
+            typeof(CornerRadius),
+            typeof(ListViewEx),
+            new PropertyMetadata(new CornerRadius(4)));
+
+        public static readonly DependencyProperty ForegroundInactiveProperty = DependencyProperty.Register(
+            nameof(ForegroundInactive),
+            typeof(Brush),
+            typeof(ListViewEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.LightForeground)));
+
+        public static readonly DependencyProperty OpacityInactiveProperty = DependencyProperty.Register(
+            nameof(OpacityInactive),
+            typeof(double),
+            typeof(ListViewEx),
+            new PropertyMetadata(0.56d));
+
+        public static readonly DependencyProperty ScrollViewerStyleProperty = DependencyProperty.Register(
+            nameof(ScrollViewerStyle),
+            typeof(Style),
+            typeof(ListViewEx),
+            new PropertyMetadata(GetGenericScrollViewerExStyle()));
 
 
         //  GETTERS & SETTERS
 
+        public Brush BackgroundInactive
+        {
+            get => (Brush)GetValue(BackgroundInactiveProperty);
+            set => SetValue(BackgroundInactiveProperty, value);
+        }
 
+        public Brush BorderBrushInactive
+        {
+            get => (Brush)GetValue(BorderBrushInactiveProperty);
+            set => SetValue(BorderBrushInactiveProperty, value);
+        }
+
+        public CornerRadius CornerRadius
+        {
+            get => (CornerRadius)GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+
+        public Brush ForegroundInactive
+        {
+            get => (Brush)GetValue(ForegroundInactiveProperty);
+            set => SetValue(ForegroundInactiveProperty, value);
+        }
+
+        public double OpacityInactive
+        {
+            get => (double)GetValue(OpacityInactiveProperty);
+            set => SetValue(OpacityInactiveProperty, MathUtilities.Clamp(value, 0d, 1d));
+        }
+
+        public Style ScrollViewerStyle
+        {
+            get => (Style)GetValue(ScrollViewerStyleProperty);
+            set => SetValue(ScrollViewerStyleProperty, value);
+        }
 
 
         //  METHODS
@@ -34,6 +105,20 @@ namespace chkam05.Tools.ControlsEx
         }
 
         #endregion CONSTRUCTORS
+
+        #region STYLES
+
+        //  --------------------------------------------------------------------------------
+        /// <summary> Get generic ScrollBarEx style from resources. </summary>
+        /// <returns> ScrollBarEx style. </returns>
+        protected static Style GetGenericScrollViewerExStyle()
+        {
+            var uri = new Uri("pack://application:,,,/chkam05.Tools.ControlsEx;component/Themes/ScrollViewerEx.xaml", UriKind.Absolute);
+            var resourceDictionary = new ResourceDictionary { Source = uri };
+            return resourceDictionary["ScrollViewerExStyle"] as Style;
+        }
+
+        #endregion STYLES
 
     }
 }
--- NetFramework/chkam05.Tools.ControlsEx/ListViewItemEx.cs
+++ NetFramework/chkam05.Tools.ControlsEx/ListViewItemEx.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using chkam05.Tools.ControlsEx.Resources;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace chkam05.Tools.ControlsEx
 {
@@ -13,7 +15,7 @@ namespace chkam05.Tools.ControlsEx
 
         //  DEPENDENCY PROPERTIES
 
-
+        
 
 
         //  GETTERS & SETTERS
