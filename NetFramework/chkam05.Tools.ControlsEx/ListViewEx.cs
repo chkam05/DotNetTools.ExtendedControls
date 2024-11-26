246d7e54d2aeb0bce4104ce168c21cc3c14ed467
Kamil Karpiński <kamil.k3@outlook.com>
2024-11-26 15:46:54 +0100

    Started working on ListViewEx and ListViewItemEx

---
+++ NetFramework/chkam05.Tools.ControlsEx/ListViewEx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace chkam05.Tools.ControlsEx
+{
+    public class ListViewEx : ListView
+    {
+
+        //  DEPENDENCY PROPERTIES
+
+
+
+
+        //  GETTERS & SETTERS
+
+
+
+
+        //  METHODS
+
+        #region CONSTRUCTORS
+
+        //  --------------------------------------------------------------------------------
+        /// <summary> ListViewEx class constructor. </summary>
+        static ListViewEx()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ListViewEx),
+                new FrameworkPropertyMetadata(typeof(ListViewEx)));
+        }
+
+        #endregion CONSTRUCTORS
+
+    }
+}
---
+++ NetFramework/chkam05.Tools.ControlsEx/ListViewItemEx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace chkam05.Tools.ControlsEx
+{
+    public class ListViewItemEx : ListViewItem
+    {
+
+        //  DEPENDENCY PROPERTIES
+
+
+
+
+        //  GETTERS & SETTERS
+
+
+
+
+        //  METHODS
+
+        #region CONSTRUCTORS
+
+        //  --------------------------------------------------------------------------------
+        /// <summary> ListViewItemEx class constructor. </summary>
+        static ListViewItemEx()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ListViewItemEx),
+                new FrameworkPropertyMetadata(typeof(ListViewItemEx)));
+        }
+
+        #endregion CONSTRUCTORS
+
+    }
+}
