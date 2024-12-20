cbf5d27afc5be81e3041895d0ffbd3797536a12b
Kamil Karpiński <kamil.k3@outlook.com>
2024-12-20 23:46:16 +0100

    Working on Calendar

---
+++ NetFramework/chkam05.Tools.ControlsEx/CalendarEx.cs
@@ -0,0 +1,181 @@
+﻿using chkam05.Tools.ControlsEx.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace chkam05.Tools.ControlsEx
+{
+    public class CalendarEx : Calendar
+    {
+
+        //  DEPENDENCY PROPERTIES
+
+        public static readonly DependencyProperty BackgroundInactiveProperty = DependencyProperty.Register(
+            nameof(BackgroundInactive),
+            typeof(Brush),
+            typeof(CalendarEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.LighInactive)));
+
+        public static readonly DependencyProperty BorderBrushInactiveProperty = DependencyProperty.Register(
+            nameof(BorderBrushInactive),
+            typeof(Brush),
+            typeof(CalendarEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.LighInactive)));
+
+        public static readonly DependencyProperty ContentMarginProperty = DependencyProperty.Register(
+            nameof(ContentMargin),
+            typeof(Thickness),
+            typeof(CalendarEx),
+            new PropertyMetadata(new Thickness(0,4,0,0)));
+
+        public static readonly DependencyProperty ContentPaddingProperty = DependencyProperty.Register(
+            nameof(ContentPadding),
+            typeof(Thickness),
+            typeof(CalendarEx),
+            new PropertyMetadata(new Thickness(0)));
+
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register(
+            nameof(CornerRadius),
+            typeof(CornerRadius),
+            typeof(CalendarEx),
+            new PropertyMetadata(new CornerRadius(4)));
+
+        //  DayMonthYearButton
+
+        /*public static readonly DependencyProperty DayMonthYearButtonBackgroundProperty = DependencyProperty.Register(
+            nameof(DayMonthYearButtonBackground),
+            typeof(Brush),
+            typeof(CalendarEx),
+            new PropertyMetadata(new SolidColorBrush(Colors.Transparent)));*/
+
+        public static readonly DependencyProperty ForegroundInactiveProperty = DependencyProperty.Register(
+            nameof(ForegroundInactive),
+            typeof(Brush),
+            typeof(CalendarEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.DarkInactive)));
+
+        public static readonly DependencyProperty HeaderBackgroundProperty = DependencyProperty.Register(
+            nameof(HeaderBackground),
+            typeof(Brush),
+            typeof(CalendarEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.LightShadeBackground)));
+
+        public static readonly DependencyProperty HeaderBorderBrushProperty = DependencyProperty.Register(
+            nameof(HeaderBorderBrush),
+            typeof(Brush),
+            typeof(CalendarEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.LightShadeBackground)));
+
+        public static readonly DependencyProperty HeaderBorderThicknessProperty = DependencyProperty.Register(
+            nameof(HeaderBorderThickness),
+            typeof(Thickness),
+            typeof(CalendarEx),
+            new PropertyMetadata(new Thickness(1)));
+
+        public static readonly DependencyProperty HeaderMarginProperty = DependencyProperty.Register(
+            nameof(HeaderMargin),
+            typeof(Thickness),
+            typeof(CalendarEx),
+            new PropertyMetadata(new Thickness(0)));
+
+        //  HeaderNavigationButton
+
+        public static readonly DependencyProperty HeaderPaddingProperty = DependencyProperty.Register(
+            nameof(HeaderPadding),
+            typeof(Thickness),
+            typeof(CalendarEx),
+            new PropertyMetadata(new Thickness(1)));
+
+        //  HeaderTitleButton
+
+
+        //  GETTERS & SETTERS
+
+        public Brush BackgroundInactive
+        {
+            get => (Brush)GetValue(BackgroundInactiveProperty);
+            set => SetValue(BackgroundInactiveProperty, value);
+        }
+
+        public Brush BorderBrushInactive
+        {
+            get => (Brush)GetValue(BorderBrushInactiveProperty);
+            set => SetValue(BorderBrushInactiveProperty, value);
+        }
+
+        public Thickness ContentMargin
+        {
+            get => (Thickness)GetValue(ContentMarginProperty);
+            set => SetValue(ContentMarginProperty, value);
+        }
+
+        public Thickness ContentPadding
+        {
+            get => (Thickness)GetValue(ContentPaddingProperty);
+            set => SetValue(ContentPaddingProperty, value);
+        }
+
+        public CornerRadius CornerRadius
+        {
+            get => (CornerRadius)GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+
+        public Brush ForegroundInactive
+        {
+            get => (Brush)GetValue(ForegroundInactiveProperty);
+            set => SetValue(ForegroundInactiveProperty, value);
+        }
+
+        public Brush HeaderBackground
+        {
+            get => (Brush)GetValue(HeaderBackgroundProperty);
+            set => SetValue(HeaderBackgroundProperty, value);
+        }
+
+        public Brush HeaderBorderBrush
+        {
+            get => (Brush)GetValue(HeaderBorderBrushProperty);
+            set => SetValue(HeaderBorderBrushProperty, value);
+        }
+
+        public Thickness HeaderBorderThickness
+        {
+            get => (Thickness)GetValue(HeaderBorderThicknessProperty);
+            set => SetValue(HeaderBorderThicknessProperty, value);
+        }
+
+        public Thickness HeaderMargin
+        {
+            get => (Thickness)GetValue(HeaderMarginProperty);
+            set => SetValue(HeaderMarginProperty, value);
+        }
+
+        public Thickness HeaderPadding
+        {
+            get => (Thickness)GetValue(HeaderPaddingProperty);
+            set => SetValue(HeaderPaddingProperty, value);
+        }
+
+
+        //  METHODS
+
+        #region CONSTRUCTORS
+
+        //  --------------------------------------------------------------------------------
+        /// <summary> CalendarEx class constructor. </summary>
+        static CalendarEx()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CalendarEx),
+                new FrameworkPropertyMetadata(typeof(CalendarEx)));
+        }
+
+        #endregion CONSTRUCTORS
+
+    }
+}
