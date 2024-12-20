493f1b924c951dcc9d112db15729d4c02f6eddfc
Kamil Karpiński <kamil.k3@outlook.com>
2024-12-20 02:50:38 +0100

    Added ListBoxEx, ListBoxItemEx

---
+++ NetFramework/chkam05.Tools.ControlsEx/ListBoxEx.cs
@@ -0,0 +1,136 @@
+﻿using chkam05.Tools.ControlsEx.Resources;
+using chkam05.Tools.ControlsEx.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace chkam05.Tools.ControlsEx
+{
+    public class ListBoxEx : ListBox
+    {
+
+        //  DEPENDENCY PROPERTIES
+
+        public static readonly DependencyProperty BackgroundInactiveProperty = DependencyProperty.Register(
+            nameof(BackgroundInactive),
+            typeof(Brush),
+            typeof(ListBoxEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.LighInactive)));
+
+        public static readonly DependencyProperty BorderBrushInactiveProperty = DependencyProperty.Register(
+            nameof(BorderBrushInactive),
+            typeof(Brush),
+            typeof(ListBoxEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.LighInactive)));
+
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register(
+            nameof(CornerRadius),
+            typeof(CornerRadius),
+            typeof(ListBoxEx),
+            new PropertyMetadata(new CornerRadius(4)));
+
+        public static readonly DependencyProperty ForegroundInactiveProperty = DependencyProperty.Register(
+            nameof(ForegroundInactive),
+            typeof(Brush),
+            typeof(ListBoxEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.LightForeground)));
+
+        public static readonly DependencyProperty OpacityInactiveProperty = DependencyProperty.Register(
+            nameof(OpacityInactive),
+            typeof(double),
+            typeof(ListBoxEx),
+            new PropertyMetadata(0.56d));
+
+        public static readonly DependencyProperty ScrollViewerStyleProperty = DependencyProperty.Register(
+            nameof(ScrollViewerStyle),
+            typeof(Style),
+            typeof(ListBoxEx),
+            new PropertyMetadata(GetGenericScrollViewerExStyle()));
+
+
+        //  GETTERS & SETTERS
+
+        public Brush BackgroundInactive
+        {
+            get => (Brush)GetValue(BackgroundInactiveProperty);
+            set => SetValue(BackgroundInactiveProperty, value);
+        }
+
+        public Brush BorderBrushInactive
+        {
+            get => (Brush)GetValue(BorderBrushInactiveProperty);
+            set => SetValue(BorderBrushInactiveProperty, value);
+        }
+
+        public CornerRadius CornerRadius
+        {
+            get => (CornerRadius)GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+
+        public Brush ForegroundInactive
+        {
+            get => (Brush)GetValue(ForegroundInactiveProperty);
+            set => SetValue(ForegroundInactiveProperty, value);
+        }
+
+        public double OpacityInactive
+        {
+            get => (double)GetValue(OpacityInactiveProperty);
+            set => SetValue(OpacityInactiveProperty, MathUtilities.Clamp(value, 0d, 1d));
+        }
+
+        public Style ScrollViewerStyle
+        {
+            get => (Style)GetValue(ScrollViewerStyleProperty);
+            set => SetValue(ScrollViewerStyleProperty, value);
+        }
+
+
+        //  METHODS
+
+        #region CONSTRUCTORS
+
+        //  --------------------------------------------------------------------------------
+        /// <summary> ListBoxEx class constructor. </summary>
+        static ListBoxEx()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ListBoxEx),
+                new FrameworkPropertyMetadata(typeof(ListBoxEx)));
+        }
+
+        #endregion CONSTRUCTORS
+
+        #region ITEMS
+
+        //  --------------------------------------------------------------------------------
+        /// <summary> Creates or identifies the element used to display the specified item. </summary>
+        /// <returns> The element used to display the specified item. </returns>
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new ListBoxItemEx();
+        }
+
+        #endregion ITEMS
+
+        #region STYLES
+
+        //  --------------------------------------------------------------------------------
+        /// <summary> Get generic ScrollBarEx style from resources. </summary>
+        /// <returns> ScrollBarEx style. </returns>
+        protected static Style GetGenericScrollViewerExStyle()
+        {
+            var uri = new Uri("pack://application:,,,/chkam05.Tools.ControlsEx;component/Themes/ScrollViewerEx.xaml", UriKind.Absolute);
+            var resourceDictionary = new ResourceDictionary { Source = uri };
+            return resourceDictionary["ScrollViewerExStyle"] as Style;
+        }
+
+        #endregion STYLES
+
+    }
+}
