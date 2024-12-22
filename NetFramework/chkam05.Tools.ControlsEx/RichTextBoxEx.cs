51053f643bbc0195184547accff36bb4054433ff
Kamil Karpiński <kamil.k3@outlook.com>
2024-12-22 23:10:18 +0100

    Added RichTextBoxEx; Updated TextBoxEx

---
+++ NetFramework/chkam05.Tools.ControlsEx/RichTextBoxEx.cs
@@ -0,0 +1,194 @@
+﻿using chkam05.Tools.ControlsEx.Resources;
+using chkam05.Tools.ControlsEx.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace chkam05.Tools.ControlsEx
+{
+    public class RichTextBoxEx : RichTextBox
+    {
+
+        //  DEPENDENCY PROPERTIES
+
+        public static readonly DependencyProperty BackgroundInactiveProperty = DependencyProperty.Register(
+            nameof(BackgroundInactive),
+            typeof(Brush),
+            typeof(RichTextBoxEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.LighInactive)));
+
+        public static readonly DependencyProperty BackgroundMouseOverProperty = DependencyProperty.Register(
+            nameof(BackgroundMouseOver),
+            typeof(Brush),
+            typeof(RichTextBoxEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.LightBackground)));
+
+        public static readonly DependencyProperty BackgroundSelectedProperty = DependencyProperty.Register(
+            nameof(BackgroundSelected),
+            typeof(Brush),
+            typeof(RichTextBoxEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.LightBackground)));
+
+        public static readonly DependencyProperty BorderBrushInactiveProperty = DependencyProperty.Register(
+            nameof(BorderBrushInactive),
+            typeof(Brush),
+            typeof(RichTextBoxEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.LighInactive)));
+
+        public static readonly DependencyProperty BorderBrushMouseOverProperty = DependencyProperty.Register(
+            nameof(BorderBrushMouseOver),
+            typeof(Brush),
+            typeof(RichTextBoxEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.DefaultAccentColorMouseOver)));
+
+        public static readonly DependencyProperty BorderBrushSelectedProperty = DependencyProperty.Register(
+            nameof(BorderBrushSelected),
+            typeof(Brush),
+            typeof(RichTextBoxEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.DefaultAccentColorPressed)));
+
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register(
+            nameof(CornerRadius),
+            typeof(CornerRadius),
+            typeof(RichTextBoxEx),
+            new PropertyMetadata(new CornerRadius(4)));
+
+        public static readonly DependencyProperty ForegroundInactiveProperty = DependencyProperty.Register(
+            nameof(ForegroundInactive),
+            typeof(Brush),
+            typeof(RichTextBoxEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.LighInactive)));
+
+        public static readonly DependencyProperty ForegroundMouseOverProperty = DependencyProperty.Register(
+            nameof(ForegroundMouseOver),
+            typeof(Brush),
+            typeof(RichTextBoxEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.LightForeground)));
+
+        public static readonly DependencyProperty ForegroundSelectedProperty = DependencyProperty.Register(
+            nameof(ForegroundSelected),
+            typeof(Brush),
+            typeof(RichTextBoxEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.LightForeground)));
+
+        public static readonly DependencyProperty HyperlinkBrushProperty = DependencyProperty.Register(
+            nameof(HyperlinkBrush),
+            typeof(Brush),
+            typeof(RichTextBoxEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.DefaultAccentColor)));
+
+        public static readonly DependencyProperty HyperlinkBrushMouseOverProperty = DependencyProperty.Register(
+            nameof(HyperlinkBrushMouseOver),
+            typeof(Brush),
+            typeof(RichTextBoxEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.DarkMouseOver)));
+
+        public static readonly DependencyProperty OpacityInactiveProperty = DependencyProperty.Register(
+            nameof(OpacityInactive),
+            typeof(double),
+            typeof(RichTextBoxEx),
+            new PropertyMetadata(0.56d));
+
+
+        //  GETTERS & SETTERS
+
+        public Brush BackgroundInactive
+        {
+            get => (Brush)GetValue(BackgroundInactiveProperty);
+            set => SetValue(BackgroundInactiveProperty, value);
+        }
+
+        public Brush BackgroundMouseOver
+        {
+            get => (Brush)GetValue(BackgroundMouseOverProperty);
+            set => SetValue(BackgroundMouseOverProperty, value);
+        }
+
+        public Brush BackgroundSelected
+        {
+            get => (Brush)GetValue(BackgroundSelectedProperty);
+            set => SetValue(BackgroundSelectedProperty, value);
+        }
+
+        public Brush BorderBrushInactive
+        {
+            get => (Brush)GetValue(BorderBrushInactiveProperty);
+            set => SetValue(BorderBrushInactiveProperty, value);
+        }
+
+        public Brush BorderBrushMouseOver
+        {
+            get => (Brush)GetValue(BorderBrushMouseOverProperty);
+            set => SetValue(BorderBrushMouseOverProperty, value);
+        }
+
+        public Brush BorderBrushSelected
+        {
+            get => (Brush)GetValue(BorderBrushSelectedProperty);
+            set => SetValue(BorderBrushSelectedProperty, value);
+        }
+
+        public CornerRadius CornerRadius
+        {
+            get => (CornerRadius)GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+
+        public Brush ForegroundInactive
+        {
+            get => (Brush)GetValue(ForegroundInactiveProperty);
+            set => SetValue(ForegroundInactiveProperty, value);
+        }
+
+        public Brush ForegroundMouseOver
+        {
+            get => (Brush)GetValue(ForegroundMouseOverProperty);
+            set => SetValue(ForegroundMouseOverProperty, value);
+        }
+
+        public Brush ForegroundSelected
+        {
+            get => (Brush)GetValue(ForegroundSelectedProperty);
+            set => SetValue(ForegroundSelectedProperty, value);
+        }
+
+        public Brush HyperlinkBrush
+        {
+            get => (Brush)GetValue(HyperlinkBrushProperty);
+            set => SetValue(HyperlinkBrushProperty, value);
+        }
+
+        public Brush HyperlinkBrushMouseOver
+        {
+            get => (Brush)GetValue(HyperlinkBrushMouseOverProperty);
+            set => SetValue(HyperlinkBrushMouseOverProperty, value);
+        }
+
+        public double OpacityInactive
+        {
+            get => (double)GetValue(OpacityInactiveProperty);
+            set => SetValue(OpacityInactiveProperty, MathUtilities.Clamp(value, 0d, 1d));
+        }
+
+
+        //  METHODS
+
+        #region CONSTRUCTORS
+
+        //  --------------------------------------------------------------------------------
+        /// <summary> RichTextBoxEx class constructor. </summary>
+        static RichTextBoxEx()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(RichTextBoxEx),
+                new FrameworkPropertyMetadata(typeof(RichTextBoxEx)));
+        }
+
+        #endregion CONSTRUCTORS
+
+    }
+}
