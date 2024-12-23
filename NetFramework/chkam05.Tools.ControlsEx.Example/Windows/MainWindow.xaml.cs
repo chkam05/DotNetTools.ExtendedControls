97f69f02c7d87acede2d5d2862f721e6c1759b7f
Kamil Karpiński <kamil.k3@outlook.com>
2024-12-24 00:25:20 +0100

    Working on HamburgerMenuEx; Corrected ListViewEx

--- NetFramework/chkam05.Tools.ControlsEx.Example/Windows/MainWindow.xaml.cs
+++ NetFramework/chkam05.Tools.ControlsEx.Example/Windows/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using chkam05.Tools.ControlsEx.ViewModels;
+using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,11 +18,53 @@ using System.Windows.Shapes;
 
 namespace chkam05.Tools.ControlsEx.Example.Windows
 {
-    public partial class MainWindow : WindowEx
+    public partial class MainWindow : WindowEx, INotifyPropertyChanged
     {
+
+        //  EVENTS
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        //  VARIABLES
+
+        private ObservableCollection<HamburgerMenuItemViewModel> hamburgerMenuItems;
+
+
+        //  GETTERS & SETTERS
+
+        public ObservableCollection<HamburgerMenuItemViewModel> HamburgerMenuItems
+        {
+            get => hamburgerMenuItems;
+            set
+            {
+                hamburgerMenuItems = value;
+                hamburgerMenuItems.CollectionChanged += (s, e)
+                    => NotifyPropertyChanged(nameof(HamburgerMenuItems));
+                NotifyPropertyChanged(nameof(HamburgerMenuItems));
+            }
+        }
+
+
+        //  METHODS
+
         public MainWindow(object args)
         {
             InitializeComponent();
+
+            HamburgerMenuItems = new ObservableCollection<HamburgerMenuItemViewModel>()
+            {
+                HamburgerMenuItemViewModel.CreateHeaderItem(),
+                new HamburgerMenuItemViewModel("Home", PackIconKind.Home, "Show home page"),
+                new HamburgerMenuItemViewModel("Settings", PackIconKind.Gear, "Show settings page"),
+                new HamburgerMenuItemViewModel("Info", PackIconKind.InfoBox, "Show info page"),
+            };
         }
+
+        protected virtual void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
     }
 }
---
+++ NetFramework/chkam05.Tools.ControlsEx/Data/HamburgerMenuItemType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chkam05.Tools.ControlsEx.Data
+{
+    public enum HamburgerMenuItemType
+    {
+        Regular = 0,
+        Header = 1,
+        NavigationBack = 2
+    }
+}
