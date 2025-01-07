48a676f6f167e302bab53c42698fceb788776b74
Kamil Karpiński <kamil.k3@outlook.com>
2025-01-07 23:37:54 +0100

    Added GroupBoxEx

---
+++ NetFramework/chkam05.Tools.ControlsEx/Converters/GroupBoxExHeaderMarginConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace chkam05.Tools.ControlsEx.Converters
+{
+    public class GroupBoxExHeaderMarginConverter : IValueConverter
+    {
+
+        //  METHODS
+
+        //  --------------------------------------------------------------------------------
+        /// <summary> Returns the GroupBoxEx header margin value. </summary>
+        /// <param name="value"> GroupBoxEx header height produced by the binding source. </param>
+        /// <param name="targetType"> The type of the binding target property. </param>
+        /// <param name="parameter"> The converter parameter to use for compare. </param>
+        /// <param name="culture"> The culture to use in converter. </param>
+        /// <returns> GroupBoxEx header margin value. </returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double height)
+                return new Thickness(0, 0, 0, -(height / 2));
+
+            return new Thickness(0);
+        }
+
+        //  --------------------------------------------------------------------------------
+        /// <summary> Converts a value. </summary>
+        /// <param name="value"> The value that is produced by the binding target. </param>
+        /// <param name="targetType"> The type to convert to. </param>
+        /// <param name="parameter"> The converter parameter to use. </param>
+        /// <param name="culture"> The culture to use in the converter. </param>
+        /// <returns> A converted value. If the method returns null, the valid null value is used. </returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ NetFramework/chkam05.Tools.ControlsEx/GroupBoxEx.cs
@@ -0,0 +1,109 @@
+﻿using chkam05.Tools.ControlsEx.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace chkam05.Tools.ControlsEx
+{
+    public class GroupBoxEx : GroupBox
+    {
+
+        //  DEPENDENCY PROPERTIES
+
+        public static readonly DependencyProperty BackgroundInactiveProperty = DependencyProperty.Register(
+            nameof(BackgroundInactive),
+            typeof(Brush),
+            typeof(GroupBoxEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.LightInactive)));
+
+        public static readonly DependencyProperty BorderBrushInactiveProperty = DependencyProperty.Register(
+            nameof(BorderBrushInactive),
+            typeof(Brush),
+            typeof(GroupBoxEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.LightInactive)));
+
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register(
+            nameof(CornerRadius),
+            typeof(CornerRadius),
+            typeof(GroupBoxEx),
+            new PropertyMetadata(new CornerRadius(4)));
+
+        public static readonly DependencyProperty HeaderForegroundProperty = DependencyProperty.Register(
+            nameof(HeaderForeground),
+            typeof(Brush),
+            typeof(GroupBoxEx),
+            new PropertyMetadata(new SolidColorBrush(ColorsResources.LightForeground)));
+
+        public static readonly DependencyProperty HeaderMarginProperty = DependencyProperty.Register(
+            nameof(HeaderMargin),
+            typeof(Thickness),
+            typeof(GroupBoxEx),
+            new PropertyMetadata(new Thickness(8,0,0,0)));
+
+        public static readonly DependencyProperty HeaderPaddingProperty = DependencyProperty.Register(
+            nameof(HeaderPadding),
+            typeof(Thickness),
+            typeof(GroupBoxEx),
+            new PropertyMetadata(new Thickness(2)));
+
+
+        //  GETTERS & SETTERS
+
+        public Brush BackgroundInactive
+        {
+            get => (Brush)GetValue(BackgroundInactiveProperty);
+            set => SetValue(BackgroundInactiveProperty, value);
+        }
+
+        public Brush BorderBrushInactive
+        {
+            get => (Brush)GetValue(BorderBrushInactiveProperty);
+            set => SetValue(BorderBrushInactiveProperty, value);
+        }
+
+        public CornerRadius CornerRadius
+        {
+            get => (CornerRadius)GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+
+        public Brush HeaderForeground
+        {
+            get => (Brush)GetValue(HeaderForegroundProperty);
+            set => SetValue(HeaderForegroundProperty, value);
+        }
+
+        public Thickness HeaderMargin
+        {
+            get => (Thickness)GetValue(HeaderMarginProperty);
+            set => SetValue(HeaderMarginProperty, value);
+        }
+
+        public Thickness HeaderPadding
+        {
+            get => (Thickness)GetValue(HeaderPaddingProperty);
+            set => SetValue(HeaderPaddingProperty, value);
+        }
+
+
+        //  METHODS
+
+        #region CONSTRUCTORS
+
+        //  --------------------------------------------------------------------------------
+        /// <summary> GroupBoxEx class constructor. </summary>
+        static GroupBoxEx()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(GroupBoxEx),
+                new FrameworkPropertyMetadata(typeof(GroupBoxEx)));
+        }
+
+        #endregion CONSTRUCTORS
+
+    }
+}
